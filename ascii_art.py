d1abe66a4f2c6490d9771a1630f11351f8dea5a5
Crabmann2025 <1990.c@gmx.de>
2025-08-14 14:36:57 +0200

    Extract ASCII art STAGES into ascii_art.py

---
+++ ascii_art.py
@@ -0,0 +1,30 @@
+
+# Snowman ASCII-Art-Stufen
+STAGES = [
+    # Stufe 0: Vollständiger Schneemann
+    """
+      ___  
+     /___\\ 
+     (o o) 
+     ( : ) 
+     ( : ) 
+    """,
+    # Stufe 1: Unterer Teil schmilzt
+    """
+      ___  
+     /___\\ 
+     (o o) 
+     ( : ) 
+    """,
+    # Stufe 2: Nur der Kopf bleibt
+    """
+      ___  
+     /___\\ 
+     (o o) 
+    """,
+    # Stufe 3: Schneemann komplett geschmolzen
+    """
+      ___  
+     /___\\ 
+    """
+]
