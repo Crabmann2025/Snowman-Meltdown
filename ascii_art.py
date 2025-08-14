3283b148bd60a0e267a77ec84532a438e4cfaf58
Crabmann2025 <1990.c@gmx.de>
2025-08-14 15:59:51 +0200

    Expand STAGES with more melting stages

--- ascii_art.py
+++ ascii_art.py
@@ -9,20 +9,41 @@ STAGES = [
      ( : ) 
      ( : ) 
     """,
-    # Stufe 1: Unterer Teil schmilzt
+    # Stufe 1: Vollständiger Schneemann ohne Knöpfe
+    """
+      ___  
+     /___\\ 
+     (o o) 
+     ( : ) 
+     (   ) 
+    """,
+    # Stufe 2: Unterer Teil schmilzt
     """
       ___  
      /___\\ 
      (o o) 
      ( : ) 
     """,
-    # Stufe 2: Nur der Kopf bleibt
+    # Stufe 3: Unterer Teil schmilzt ohne Knöpfe
+    """
+      ___  
+     /___\\ 
+     (o o) 
+     (   ) 
+    """,
+    # Stufe 4: Nur der Kopf bleibt
     """
       ___  
      /___\\ 
      (o o) 
     """,
-    # Stufe 3: Schneemann komplett geschmolzen
+    # Stufe 5: Nur der Kopf bleibt ohne Augen
+    """
+      ___  
+     /___\\ 
+     (   ) 
+    """,
+    # Stufe 6: Schneemann komplett geschmolzen
     """
       ___  
      /___\\ 
